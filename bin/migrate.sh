ccc150bc29c51a29a68908ada487724228cb87e7
zdm <zdm@softvisio.net>
2021-10-28 20:05:29 +0300

    fix: pg13

--- bin/migrate.sh
+++ bin/migrate.sh
@@ -3,21 +3,27 @@
 set -u
 set -e
 
+BACKUP=/var/lib/pgsql/data-old
+
 # move old cluster
-mv /var/lib/pgsql/data /var/lib/pgsql/data-old
+mv /var/lib/pgsql/data $BACKUP
 
 # get old cluster version
-FROM=$(cat /var/lib/pgsql/data-old/PG_VERSION)
+FROM=$(cat $BACKUP/PG_VERSION)
+
+dnf remove -y repo-pgsql
+dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 
 # install old cluster
-dnf -y install \
+dnf install -y \
     sudo \
     postgresql$FROM-server \
     postgresql$FROM-contrib \
     pg$FROM-extensions
 
-# create new cluster
-# NOTE will ask for superuser password
+dnf install -y citus_$FROM
+
+# create new cluster, will ask for postgres superuser password
 sudo -u postgres \
     $POSTGRES_HOME/bin/initdb \
     --encoding UTF8 \
@@ -28,18 +34,20 @@ sudo -u postgres \
 
 # move config files
 # NOTE this can be not quite correct, because it may contains incompatible settings
-\cp --preserve /var/lib/pgsql/data-old/postgresql.auto.conf /var/lib/pgsql/data/postgresql.auto.conf
-\cp --preserve /var/lib/pgsql/data-old/pg_hba.conf /var/lib/pgsql/data
-\cp --preserve -R /var/lib/pgsql/data-old/conf.d /var/lib/pgsql/data
+\cp --preserve $BACKUP/server.crt /var/lib/pgsql/data/server.crt
+\cp --preserve $BACKUP/server.key /var/lib/pgsql/data/server.key
+\cp --preserve $BACKUP/postgresql.auto.conf /var/lib/pgsql/data/postgresql.auto.conf
+\cp --preserve $BACKUP/pg_hba.conf /var/lib/pgsql/data
+\cp --preserve -R $BACKUP/conf.d /var/lib/pgsql/data
 \cp --preserve /var/lib/pgsql/data/postgresql.conf /var/lib/pgsql/data/conf.d/000-postgresql.conf
-\cp --preserve /var/lib/pgsql/data-old/postgresql.conf /var/lib/pgsql/data
+\cp --preserve $BACKUP/postgresql.conf /var/lib/pgsql/data
 
 # check clusters compatibility
 sudo -u postgres \
     $POSTGRES_HOME/bin/pg_upgrade \
     -b /usr/pgsql-$FROM/bin \
     -B $POSTGRES_HOME/bin \
-    -d /var/lib/pgsql/data-old \
+    -d $BACKUP \
     -D /var/lib/pgsql/data \
     --check
 
@@ -48,7 +56,7 @@ sudo -u postgres \
     $POSTGRES_HOME/bin/pg_upgrade \
     -b /usr/pgsql-$FROM/bin \
     -B $POSTGRES_HOME/bin \
-    -d /var/lib/pgsql/data-old \
+    -d $BACKUP \
     -D /var/lib/pgsql/data \
     -O "-c timescaledb.restoring=on"
 
